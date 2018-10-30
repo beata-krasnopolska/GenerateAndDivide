589d95292e959255a76bebe38b2be897c157a2a4
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-10-30 18:33:15 +0100

    Add project files.

---
+++ GenrateAndDivide/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenrateAndDivide
+{
+    class Program
+    {
+        //The Program generates two random numbers than divides a higher number by a lower number
+        
+        static void Main(string[] args)
+        {
+            var randomNumber = new RandomFinder();
+            var firstRandomNo = randomNumber.GetPseudoRandom();
+            int secRandomNo = randomNumber.GetPseudoRandom();
+            
+            double result;
+
+            if (firstRandomNo > secRandomNo)
+            {
+                result= firstRandomNo / secRandomNo;
+            }
+            else
+            {
+                result = secRandomNo / firstRandomNo;
+            }
+            Console.WriteLine("First random number: {0}", firstRandomNo);
+            Console.WriteLine("Second random number: {0}", secRandomNo);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }     
+    }
+}
